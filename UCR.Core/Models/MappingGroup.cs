7009dca2e820f5a224e8b5b9b7b25dd741066886
Stylish-Dark <Mr.Nick.Samuel@gmail.com>
2026-09-19 01:29:06 +1000

    Persist disabled mapping groups correctly

--- UCR.Core/Models/MappingGroup.cs
+++ UCR.Core/Models/MappingGroup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Xml.Serialization;
 
@@ -16,6 +17,7 @@ namespace HidWizards.UCR.Core.Models
         public string Title { get; set; }
 
         [XmlAttribute]
+        [DefaultValue(true)]
         public bool Enabled
         {
             get => _enabled;
