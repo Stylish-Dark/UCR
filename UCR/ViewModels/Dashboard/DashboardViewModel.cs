81d5e4b88de788de9e08b89d40719f0403f37551
Stylish-Dark <Mr.Nick.Samuel@gmail.com>
2026-09-25 00:43:46 +1000

    Expose global stop-all state on dashboard

--- UCR/ViewModels/Dashboard/DashboardViewModel.cs
+++ UCR/ViewModels/Dashboard/DashboardViewModel.cs
@@ -20,6 +20,7 @@ namespace HidWizards.UCR.ViewModels.Dashboard
         public Visibility ProfileDetailsActive => SelectedProfileItem != null ? Visibility.Visible : Visibility.Hidden;
         public bool CanActivateProfile => SelectedProfileItem?.Profile != null;
         public bool CanDeactivateProfile => SelectedProfileItem?.Profile?.IsActive() == true;
+        public bool CanDeactivateAllProfiles => Context != null && Context.ActiveProfiles.Count > 0;
         public ProfileDeviceListControlViewModel InputDeviceControlViewModel { get; set; }
         public ProfileDeviceListControlViewModel OutputDeviceControlViewModel { get; set; }
 
@@ -184,6 +185,7 @@ namespace HidWizards.UCR.ViewModels.Dashboard
             OnPropertyChanged(nameof(ActiveProfileBreadCrumbs));
             OnPropertyChanged(nameof(CanDeactivateProfile));
             OnPropertyChanged(nameof(CanActivateProfile));
+            OnPropertyChanged(nameof(CanDeactivateAllProfiles));
         }
 
         [NotifyPropertyChangedInvocator]
